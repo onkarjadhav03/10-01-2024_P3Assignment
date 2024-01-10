ea28469057a2b383f5822c86f13f857d3f259753
onkar <jadhavonkar333@gmail.com>
2024-01-10 14:58:34 +0530

    first commit

---
+++ Products.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    public class Products
+    {
+        public string Pname {  get; set; }
+        public float Pprice { get; set; }
+        public string Pbrand { get; set; }  
+        public DateTime Mfdate { get; set; }
+        public DateTime Exdate { get; set;}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Assignment4;
+
+List<Products> products = new List<Products>()
+{
+    new Products(){Pname="Mobile",Pprice=90000,Pbrand="iphone",Mfdate=new DateTime(day:15,month:12,year:2020),Exdate=new DateTime(day:15,month:12,year:2020)},
+    new Products(){Pname="Laptop",Pprice=80000,Pbrand="Asus",Mfdate=new DateTime(day:01,month:01,year:2024),Exdate=new DateTime(day:15,month:09,year:2050)},
+    new Products(){Pname="Earbuds",Pprice=1000,Pbrand="boat",Mfdate=new DateTime(day:15,month:12,year:2019),Exdate=new DateTime(day:31,month:12,year:2024)},
+    new Products(){Pname="Speaker",Pprice=25000,Pbrand="boat",Mfdate=new DateTime(day:02,month:06,year:2020),Exdate=new DateTime(day:21,month:12,year:2040)},
+    new Products(){Pname="Monitor",Pprice=32000,Pbrand="Dell",Mfdate=new DateTime(day:10,month:05,year:2020),Exdate=new DateTime(day:01,month:12,year:2050)}
+};
+
+Console.WriteLine("Name \t Price \t Brand \t MfDate \t\t ExpDate");
+foreach (Products pro in products)
+{
+    Console.Write(pro.Pname + "\t ");
+    Console.Write(pro.Pprice + "\t ");
+    Console.Write(pro.Pbrand + "\t ");
+    Console.Write(pro.Mfdate.ToLongDateString()+"\t\t");
+    Console.Write(pro.Exdate.ToLongDateString());
+    Console.Write("\n");
+}
+Console.ReadKey();
